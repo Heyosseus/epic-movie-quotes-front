6df8f61ba4ac554f414779df17f872ab013edf4d
heyosseus <ratiruxadzee@gmail.com>
2023-05-24 19:55:26 +0400

    feat: add not found and forbidden error pages

---
+++ src/views/errors/ErrorView.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="bg-landingBg h-screen w-full flex flex-col items-center justify-center space-y-12">
+    <IconNotFound />
+    <IconEllipse />
+    <div>
+      <h1 class="text-2xl lg:text-5xl text-center">Whoops!</h1>
+      <p class="text-sm lg:text-lg mt-6 text-center">We can't see the page you are looking for</p>
+    </div>
+    <button
+      class="py-2 px-6 bg-red-700 text-white rounded flex items-center justify-center mx-auto mt-6"
+      @click="navigateToHome"
+    >
+      Return home
+    </button>
+  </div>
+</template>
+<script setup>
+import IconNotFound from '@/components/icons/IconNotFound.vue'
+import IconEllipse from '@/components/icons/IconEllipse.vue'
+import { useRouter } from 'vue-router'
+const router = useRouter()
+
+const navigateToHome = () => {
+  router.push('/')
+}
+</script>
---
+++ src/views/errors/ForbiddenView.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="bg-landingBg h-screen w-full flex flex-col items-center justify-center space-y-12">
+    <div class="relative">
+      <IconUnion class="absolute top-20 flex items-center justify-center w-full" />
+      <IconGendalf class="z-1 relative mt-2" />
+    </div>
+
+    <div>
+      <h1 class="text-2xl lg:text-5xl text-center">You shall not pass!</h1>
+      <p class="text-sm lg:text-lg mt-6 text-center">
+        Sorry, but you don't have permission to access this page
+      </p>
+    </div>
+    <button
+      class="py-2 px-6 bg-red-700 text-white rounded flex items-center justify-center mx-auto mt-6"
+      @click="navigateToHome"
+    >
+      Return home
+    </button>
+  </div>
+</template>
+<script setup>
+import IconGendalf from '@/components/icons/IconGendalf.vue'
+import IconUnion from '../../components/icons/IconUnion.vue'
+import { useRouter } from 'vue-router'
+const router = useRouter()
+
+const navigateToHome = () => {
+  router.push('/')
+}
+</script>
