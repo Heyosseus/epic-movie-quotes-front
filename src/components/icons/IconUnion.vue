294026c15788fb68d2d9ba90430ae9b3f7bc5d96
heyosseus <ratiruxadzee@gmail.com>
2023-05-24 19:54:37 +0400

    style: add icons for error pages

---
+++ src/components/icons/IconUnion.vue
@@ -0,0 +1,29 @@
+<template>
+  <svg
+    width="235"
+    height="147"
+    viewBox="0 0 235 147"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      fill-rule="evenodd"
+      clip-rule="evenodd"
+      d="M114.56 121.826C75.0198 151.056 28.069 154.661 8.53845 129.512C-11.4478 103.776 4.84899 57.675 44.9383 26.5418C79.8451 -0.566797 121.281 -7.38873 144.605 8.28991C157.824 11.8246 171.504 17.9985 184.531 26.7864C226.61 55.1723 245.95 100.082 227.728 127.095C209.505 154.108 160.621 152.995 118.541 124.61C117.191 123.698 115.863 122.77 114.56 121.826Z"
+      fill="url(#paint0_linear_228_15576)"
+    />
+    <defs>
+      <linearGradient
+        id="paint0_linear_228_15576"
+        x1="19"
+        y1="39.5"
+        x2="223"
+        y2="147"
+        gradientUnits="userSpaceOnUse"
+      >
+        <stop stop-color="#CED4DA" />
+        <stop offset="1" stop-color="white" />
+      </linearGradient>
+    </defs>
+  </svg>
+</template>
