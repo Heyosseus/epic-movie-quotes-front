69385e0f0595bc454f861492311fbf444c7e7a3d
heyosseus <ratiruxadzee@gmail.com>
2023-05-29 11:43:29 +0400

    refactor: refactor models

--- src/components/modals/LoginModal.vue
+++ src/components/modals/LoginModal.vue
@@ -1,65 +1,78 @@
 <template>
-  <h1 class="text-xl">Log in to your account</h1>
-  <p class="text-sm text-gray-500">Welcome back! Please enter your details.</p>
-  <Form class="flex flex-col space-y-4" @submit="login">
-    <div class="flex flex-col w-96">
-      <label for="">Email</label>
-      <Field
-        type="email"
-        name="email"
-        class="py-2 px-2 rounded-md outline-0 text-black font-normal bg-[#CED4DA]"
-        :class="{ 'text-red-500': errors.email }"
-        placeholder="Enter your email"
-        v-model="email"
-        rules="required|email"
-      />
-      <ErrorMessage name="email" class="text-red-500 text-sm font-normal" />
-    </div>
-    <div class="flex flex-col">
-      <label for="">Password</label>
-      <div class="relative">
-        <Field
-          v-bind:type="showPassword ? 'text' : 'password'"
-          name="password"
-          class="py-2 px-2 rounded-md outline-0 w-full text-black font-normal bg-[#CED4DA]"
-          placeholder="Password"
-          v-model="password"
-          rules="required|min:8"
-        />
-        <IconShowPassword class="absolute right-2 top-3" @click="showPassword = !showPassword" />
-      </div>
-      <ErrorMessage name="password" class="text-red-500 text-sm font-normal" />
-    </div>
-    <div class="flex justify-between">
-      <div class="space-x-2">
-        <Field type="checkbox" name="remember" />
-        <label for="">Remember me</label>
-      </div>
+  <div
+    class="absolute w-screen h-screen flex flex-col items-center justify-center bg-transparentLandingBg"
+  >
+    <div
+      class="flex flex-col px-20 pt-6 pb-16 rounded-md items-center justify-center mx-auto my-auto bg-modal space-y-4"
+    >
+      <h1 class="text-xl">Log in to your account</h1>
+      <p class="text-sm text-gray-500">Welcome back! Please enter your details.</p>
+      <Form class="flex flex-col space-y-4" @submit="login">
+        <div class="flex flex-col w-96">
+          <label for="">Email</label>
+          <Field
+            type="email"
+            name="email"
+            class="py-2 px-2 rounded-md outline-0 text-black font-normal bg-[#CED4DA]"
+            :class="{ 'text-red-500': errors.email }"
+            placeholder="Enter your email"
+            v-model="email"
+            rules="required|email"
+          />
+          <ErrorMessage name="email" class="text-red-500 text-sm font-normal" />
+        </div>
+        <div class="flex flex-col">
+          <label for="">Password</label>
+          <div class="relative">
+            <Field
+              v-bind:type="showPassword ? 'text' : 'password'"
+              name="password"
+              class="py-2 px-2 rounded-md outline-0 w-full text-black font-normal bg-[#CED4DA]"
+              placeholder="Password"
+              v-model="password"
+              rules="required|min:8"
+            />
+            <IconShowPassword
+              class="absolute right-2 top-3"
+              @click="showPassword = !showPassword"
+            />
+          </div>
+          <ErrorMessage name="password" class="text-red-500 text-sm font-normal" />
+        </div>
+        <div class="flex justify-between">
+          <div class="space-x-2">
+            <Field type="checkbox" name="remember" />
+            <label for="">Remember me</label>
+          </div>
 
-      <a href="/forgot-password" class="underline text-blue-600">Forgot password</a>
+          <a href="/forgot-password" class="underline text-blue-600">Forgot password</a>
+        </div>
+        <div class="space-y-8">
+          <button
+            class="py-2 px-6 bg-red-700 text-white rounded-md flex w-full items-center justify-center mx-auto mt-6"
+            type="submit"
+          >
+            Sign in
+          </button>
+          <div>
+            <a
+              href="http://localhost:8000/auth/google/redirect"
+              class="border border-white py-3 flex w-full items-center justify-center mx-auto"
+            >
+              <IconGoogle class="w-6 h-6 mr-2" />
+              Sign in with Google</a
+            >
+          </div>
+          <p class="text-center mt-6">
+            Already have an account?
+            <a href="/register" class="underline text-blue-600" @click="openRegisterModal"
+              >Sign up</a
+            >
+          </p>
+        </div>
+      </Form>
     </div>
-    <div class="space-y-8">
-      <button
-        class="py-2 px-6 bg-red-700 text-white rounded-md flex w-full items-center justify-center mx-auto mt-6"
-        type="submit"
-      >
-        Sign in
-      </button>
-      <div>
-        <a
-          href="http://localhost:8000/auth/google/redirect"
-          class="border border-white py-3 flex w-full items-center justify-center mx-auto"
-        >
-          <IconGoogle class="w-6 h-6 mr-2" />
-          Sign in with Google</a
-        >
-      </div>
-      <p class="text-center mt-6">
-        Already have an account?
-        <a href="/register" class="underline text-blue-600" @click="openRegisterModal">Sign up</a>
-      </p>
-    </div>
-  </Form>
+  </div>
 </template>
 
 <script setup>
@@ -115,7 +128,7 @@ onMounted(() => {
 
 const login = () => {
   axios
-    .post('http://localhost:8000/api/login', { 
+    .post('http://localhost:8000/api/login', {
       email: email.value,
       password: password.value
     })
--- src/components/modals/RegisterModal.vue
+++ src/components/modals/RegisterModal.vue
@@ -1,88 +1,99 @@
 <template>
-  <h1 class="text-xl">Create an account</h1>
-  <p class="text-sm text-gray-500">Start your journey</p>
-  <Form class="flex flex-col space-y-4" @submit="register">
-    <div class="flex flex-col w-96">
-      <label for="">Name</label>
-      <Field
-        type="text"
-        name="name"
-        class="py-2 px-2 rounded-md outline-0 text-black font-normal bg-[#CED4DA]"
-        placeholder="At least 3 & max.15 lower case characters"
-        v-model="name"
-        rules="required|min_for_name:3|max:15|lowercase"
-      />
-      <ErrorMessage name="name" class="text-red-500 text-sm font-normal" />
-    </div>
-    <div class="flex flex-col w-96">
-      <label for="">Email</label>
-      <Field
-        type="email"
-        name="email"
-        class="py-2 px-2 rounded-md outline-0 text-black font-normal bg-[#CED4DA]"
-        placeholder="Enter your email"
-        v-model="email"
-        rules="required|email"
-      />
-      <ErrorMessage name="email" class="text-red-500 text-sm font-normal" />
-    </div>
-    <div class="flex flex-col">
-      <label for="">Password</label>
-      <div class="relative">
-        <Field
-          v-bind:type="showPassword ? 'text' : 'password'"
-          name="password"
-          class="py-2 px-2 rounded-md outline-0 w-full text-black font-normal bg-[#CED4DA]"
-          placeholder="Password"
-          v-model="password"
-          rules="required|min:8"
-        />
-        <IconShowPassword class="absolute right-2 top-3" @click="showPassword = !showPassword" />
-      </div>
+  <div
+    class="absolute w-screen h-screen flex flex-col items-center justify-center bg-transparentLandingBg"
+  >
+    <div
+      class="flex flex-col px-20 pt-6 pb-16 rounded-md items-center justify-center mx-auto my-auto bg-modal space-y-4"
+    >
+      <h1 class="text-xl">Create an account</h1>
+      <p class="text-sm text-gray-500">Start your journey</p>
+      <Form class="flex flex-col space-y-4" @submit="register">
+        <div class="flex flex-col w-96">
+          <label for="">Name</label>
+          <Field
+            type="text"
+            name="name"
+            class="py-2 px-2 rounded-md outline-0 text-black font-normal bg-[#CED4DA]"
+            placeholder="At least 3 & max.15 lower case characters"
+            v-model="name"
+            rules="required|min_for_name:3|max:15|lowercase"
+          />
+          <ErrorMessage name="name" class="text-red-500 text-sm font-normal" />
+        </div>
+        <div class="flex flex-col w-96">
+          <label for="">Email</label>
+          <Field
+            type="email"
+            name="email"
+            class="py-2 px-2 rounded-md outline-0 text-black font-normal bg-[#CED4DA]"
+            placeholder="Enter your email"
+            v-model="email"
+            rules="required|email"
+          />
+          <ErrorMessage name="email" class="text-red-500 text-sm font-normal" />
+        </div>
+        <div class="flex flex-col">
+          <label for="">Password</label>
+          <div class="relative">
+            <Field
+              v-bind:type="showPassword ? 'text' : 'password'"
+              name="password"
+              class="py-2 px-2 rounded-md outline-0 w-full text-black font-normal bg-[#CED4DA]"
+              placeholder="Password"
+              v-model="password"
+              rules="required|min:8"
+            />
+            <IconShowPassword
+              class="absolute right-2 top-3"
+              @click="showPassword = !showPassword"
+            />
+          </div>
 
-      <ErrorMessage name="password" class="text-red-500 text-sm font-normal" />
-    </div>
-    <div class="flex flex-col">
-      <label for="">Password confirmation</label>
-      <div class="relative">
-        <Field
-          v-bind:type="showPasswordConfirmation ? 'text' : 'password'"
-          name="password_confirmation"
-          class="py-2 px-2 rounded-md outline-0 text-black w-full font-normal bg-[#CED4DA]"
-          placeholder="Confirm Password"
-          v-model="password_confirmation"
-          rules="required|confirmed:password"
-        />
-        <IconShowPassword
-          class="absolute right-2 top-3"
-          @click="showPasswordConfirmation = !showPasswordConfirmation"
-        />
-      </div>
-      <ErrorMessage name="password_confirmation" class="text-red-500 text-sm font-normal" />
-    </div>
-    <div class="space-y-8">
-      <button
-        class="py-2 px-6 bg-red-700 text-white rounded-md flex w-full items-center justify-center mx-auto mt-2"
-        type="submit"
-      >
-        Get started
-      </button>
+          <ErrorMessage name="password" class="text-red-500 text-sm font-normal" />
+        </div>
+        <div class="flex flex-col">
+          <label for="">Password confirmation</label>
+          <div class="relative">
+            <Field
+              v-bind:type="showPasswordConfirmation ? 'text' : 'password'"
+              name="password_confirmation"
+              class="py-2 px-2 rounded-md outline-0 text-black w-full font-normal bg-[#CED4DA]"
+              placeholder="Confirm Password"
+              v-model="password_confirmation"
+              rules="required|confirmed:password"
+            />
+            <IconShowPassword
+              class="absolute right-2 top-3"
+              @click="showPasswordConfirmation = !showPasswordConfirmation"
+            />
+          </div>
+          <ErrorMessage name="password_confirmation" class="text-red-500 text-sm font-normal" />
+        </div>
+        <div class="space-y-8">
+          <button
+            class="py-2 px-6 bg-red-700 text-white rounded-md flex w-full items-center justify-center mx-auto mt-2"
+            type="submit"
+          >
+            Get started
+          </button>
 
-      <div>
-        <a
-          href="http://localhost:8000/auth/google/redirect"
-          class="border border-white py-3 flex w-full items-center justify-center mx-auto"
-        >
-          <IconGoogle class="w-6 h-6 mr-2" />
-          Sign up with Google</a
-        >
-      </div>
+          <div>
+            <a
+              href="http://localhost:8000/auth/google/redirect"
+              class="border border-white py-3 flex w-full items-center justify-center mx-auto"
+            >
+              <IconGoogle class="w-6 h-6 mr-2" />
+              Sign up with Google</a
+            >
+          </div>
 
-      <p class="text-center mt-6">
-        Already have an account? <a href="/login" class="underline text-blue-600">Log in</a>
-      </p>
+          <p class="text-center mt-6">
+            Already have an account? <a href="/login" class="underline text-blue-600">Log in</a>
+          </p>
+        </div>
+      </Form>
     </div>
-  </Form>
+  </div>
 </template>
 
 <script setup>
