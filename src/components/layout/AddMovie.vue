e294d90d3284bcb2b4652ea4741240fe64836948
heyosseus <ratiruxadzee@gmail.com>
2023-06-01 13:44:02 +0400

    refactor: handle  search functionality

--- src/components/layout/AddMovie.vue
+++ src/components/layout/AddMovie.vue
@@ -1,16 +1,5 @@
 <template>
   <div class="flex items-center space-x-4">
-    <Form method="GET">
-      <div class="flex items-center">
-        <IconSearch />
-        <Field
-          name="search"
-          type="text"
-          class="bg-transparent outline-0 w-36 flex items-center justify-center px-2"
-          placeholder="Search by"
-        />
-      </div>
-    </Form>
     <div class="bg-red-600 px-4 py-3 rounded flex items-center space-x-4">
       <IconAddMovie />
       <button class="w-24">
@@ -21,7 +10,5 @@
   </div>
 </template>
 <script setup>
-import IconSearch from '@/components/icons/IconSearch.vue'
 import IconAddMovie from '@/components/icons/IconAddMovie.vue'
-import { Form, Field } from 'vee-validate'
 </script>
--- src/views/feeds/MovieListView.vue
+++ src/views/feeds/MovieListView.vue
@@ -7,17 +7,30 @@
         <div class="w-full px-16 pr-56">
           <div class="flex items-center w-full justify-between mt-8 pl-4">
             <h1 class="text-lg w-full mt-10">My list of movies ({{ movies.length }})</h1>
+            <Form method="GET" @keydown.enter.prevent="handleSearch">
+              <div class="flex items-center">
+                <IconSearch @click="handleSearch" />
+                <Field
+                  name="search"
+                  type="text"
+                  class="bg-transparent outline-0 w-36 flex items-center justify-center px-2"
+                  placeholder="Search by"
+                  v-model="search"
+                />
+              </div>
+            </Form>
             <AddMovie />
-            <form @submit.prevent></form>
-            <input type="text" name="search" v-model="search" class="text-black">
-            <button @click="handleSearch" >click</button>
           </div>
 
           <div class="mt-20" v-if="movies.length > 0">
             <div class="flex flex-wrap">
               <div v-for="movie in movies" :key="movie.id" class="w-1/3 pl-6 mb-8">
                 <div class="flex flex-col items-center">
-                  <img :src="getImages(movie.poster)" alt="" class="rounded-2xl object-fit h-96 mr-auto" />
+                  <img
+                    :src="getImages(movie.poster)"
+                    alt=""
+                    class="rounded-2xl object-fit h-96 mr-auto"
+                  />
                   <div class="flex space-x-4 mr-auto mt-4 mb-10">
                     <h1 class="uppercase">{{ movie.title.en }}</h1>
                     <p class="">({{ movie.release_date }})</p>
@@ -36,6 +49,8 @@
   </div>
 </template>
 <script setup>
+import IconSearch from '@/components/icons/IconSearch.vue'
+import { Form, Field } from 'vee-validate'
 import BaseHeader from '@/components/layout/BaseHeader.vue'
 import BaseSidebar from '@/components/layout/BaseSidebar.vue'
 import AddMovie from '@/components/layout/AddMovie.vue'
@@ -75,14 +90,4 @@ onMounted(() => {
 const getImages = (poster) => {
   return `http://localhost:8000/storage/${poster}`
 }
-
-// const parseTitle = (title) => {
-//   try {
-//     const parsedTitle = JSON.parse(title)
-//     return parsedTitle.en
-//   } catch (error) {
-//     console.log('Error parsing title:', error)
-//     return ''
-//   }
-// }
 </script>
